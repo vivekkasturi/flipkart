2e50252e9225f4a2eba49cd5fd27ccf7a6cb3bfb
vivekkasturi <kasturi.vivek994@gmail.com>
2024-02-16 12:22:37 +0530

    Added test.js file

---
+++ src/Test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+Test.propTypes = {
+    
+};
+
+function Test() {
+    return (
+        <div>
+            <h1>Test file</h1>
+        </div>
+    );
+}
+
+export default Test;
